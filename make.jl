f8b13bc683da08ade819a14308907fd5fbd5e2fd
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-01-08 13:08:41 +0100

    Update make.jl

--- make.jl
+++ make.jl
@@ -6,10 +6,6 @@ import Remark
 
 files =  filter( f -> startswith(f, "0"), readdir("src")) |> collect
 
-for file in files
-    Literate.notebook("src/$file", "notebooks",  execute=true)
-end
-
 run(pipeline(`cat src/$files`; stdout="slides.jl" ))
 slides_path = joinpath("docs")
 mkpath(slides_path)
