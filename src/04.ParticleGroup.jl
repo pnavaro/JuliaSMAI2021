85003079aace96c194fad9eb507448d99c6ba698
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-01-08 19:45:35 +0100

    Update 04.ParticleGroup.jl

--- src/04.ParticleGroup.jl
+++ src/04.ParticleGroup.jl
@@ -1,19 +1,16 @@
-ENV["GKSwstype"]="100" #src
-
 # # Metaprogramming 
 #
 # ## The ParticleGroup example
 
-import Sobol
-using Plots, LinearAlgebra
 
-#     ParticleGroup{D,V}(n_particles, charge, mass)
-#
-# - `D` : number of dimension in physical space
-# - `V` : number of dimension in phase space
-# - `n` : number of particles
-# 
-mutable struct ParticleGroup{D,V}
+"""
+    ParticleGroup{D,V}(n_particles, charge, mass)
+
+ - `D` : number of dimension in physical space
+ - `V` : number of dimension in phase space
+ - `n` : number of particles
+""" 
+struct ParticleGroup{D,V}
 
     n_particles       :: Int64
     data              :: Array{Float64, 2}
@@ -23,14 +20,17 @@ mutable struct ParticleGroup{D,V}
         data = zeros( Float64, (D+V, n))
         new( n, data) 
     end
+
 end
 
 #md # ---
 
+
 # 
 # Set position of ith particle of p to x
 # 
-@generated function set_x!( p :: ParticleGroup{D,V}, i, x :: Float64 ) where {D, V}
+@generated function set_x!( p :: ParticleGroup{D,V}, i, 
+                            x :: Float64 ) where {D, V}
 
     :(p.data[1, i] = x)
 
@@ -41,7 +41,8 @@ end
 # 
 # Set position of ith particle of p to x when x is a vector
 # 
-@generated function set_x!( p :: ParticleGroup{D,V}, i, x :: Vector{Float64} ) where {D, V}
+@generated function set_x!( p :: ParticleGroup{D,V}, i, 
+                            x :: Vector{Float64} ) where {D, V}
 
      :(for j in 1:$D p.data[j, i] = x[j] end)
 
@@ -52,7 +53,8 @@ end
 #
 # Set velocity of ith particle of p to v
 #
-@generated function set_v!( p :: ParticleGroup{D,V}, i, v :: Float64 ) where {D, V}
+@generated function set_v!( p :: ParticleGroup{D,V}, i,
+                            v :: Float64 ) where {D, V}
 
     :(p.data[$D+1, i] = v)
 
@@ -63,7 +65,8 @@ end
 # 
 # Set velocity of ith particle of p to v
 # 
-@generated function set_v!( p :: ParticleGroup{D,V}, i, v :: Vector{Float64} ) where {D, V}
+@generated function set_v!( p :: ParticleGroup{D,V}, i,        
+                            v :: Vector{Float64} ) where {D, V}
 
     :(for j in 1:$V p.data[$D+j, i] = v[j] end)
 
@@ -90,9 +93,10 @@ end
 
 #md # ---
 
-# 
-# Sampling from a probability distribution to initialize Landau damping
-# 
+import Sobol
+ 
+# f(x,v) = 1/2π (1 + α cos (kx * x) * exp(-(vx^2+vy^2))
+
 function landau_sampling!( pg :: ParticleGroup{1,2}, alpha, kx )
     
     function newton(r)
@@ -110,7 +114,6 @@ function landau_sampling!( pg :: ParticleGroup{1,2}, alpha, kx )
     nbpart = pg.n_particles
 
     for i=1:nbpart
-
         v = sqrt(-2 * log( (i-0.5)/nbpart))
         r1, r2 = Sobol.next!(s)
         θ = r1 * 2π
@@ -136,6 +139,9 @@ vp = vcat([get_v(pg, i) for i in 1:pg.n_particles]'...)
 
 #md # ---
 #nb #+
+
+using Plots
+
 pp = plot(layout=(3,1))
 histogram!(pp[1,1], xp, normalize=true, bins = 100, lab=:x)
 plot!(pp[1,1], x -> (1+alpha*cos(kx*x))/(2π/kx), 0., 2π/kx, lab="")
@@ -155,6 +161,4 @@ histogram2d(vp[:,1], vp[:,2], normalize=true, bins=100)
 
 #md # ![](hist2d.svg)
 
-@test true #src
-
-#md # Next: [GEMPIC](/Numkin2019/05/build/index.html)
+# ---
