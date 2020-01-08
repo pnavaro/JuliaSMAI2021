193ee1c545032bd13a2781e72521bfde24e911a3
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-01-08 20:10:46 +0100

    Update generate_nb.jl

--- generate_nb.jl
+++ generate_nb.jl
@@ -4,5 +4,5 @@ using  Plots
 files =  filter( f -> startswith(f, "0"), readdir("src")) |> collect
 
 for file in files
-    Literate.notebook("src/$file", "notebooks",  execute=true)
+    Literate.notebook("src/$file", "notebooks",  execute=false)
 end
